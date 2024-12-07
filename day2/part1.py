e4f3d07c44582bb01aa15753510c1d60b8390a27
Sophie Marshall-Unitt <75947775+sophielmu@users.noreply.github.com>
2024-12-07 21:59:51 +0000

    Day 2 part 1

--- day2/part1.py
+++ day2/part1.py
@@ -1,9 +1,30 @@
 number_of_safe_reports = 0
 
 if __name__ == "__main__":
-       with open("day1/input.txt", "r") as f:
+       with open("day2/input.txt", "r") as f:
         for line in f:
             report = [int(x) for x in line.split()]
+            signs_between_levels = []
+
+            for i in range(1, len(report)):
+                difference = report[i - 1] - report[i]
+                if abs(difference) > 3:
+                    break
+                if difference == 0:
+                    break
+
+                if report[i] > report[i - 1]:
+                    signs_between_levels.append("+")
+                else:
+                    signs_between_levels.append("-")
+
+            if len(signs_between_levels) == 0:
+                continue
+            all_signs_are_the_same = signs_between_levels.count(signs_between_levels[0]) == len(report) - 1
+            if all_signs_are_the_same:
+                number_of_safe_reports += 1
+
+        print(number_of_safe_reports)
 
 
 

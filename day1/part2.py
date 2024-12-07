7ecf3897febe3dd6a609e9eb0ace7c98d57a97ca
sophielmu <sophiemarshall-unitt@hotmail.com>
2024-12-07 19:58:32 +0000

    Day 1 part 2

--- day1/part2.py
+++ day1/part2.py
@@ -1,6 +1,6 @@
 list_1 = []
 list_2 = []
-distance = 0
+similarity_score = 0
 
 def read_input():
     with open("day1/input.txt", "r") as f:
@@ -8,13 +8,12 @@ def read_input():
             s = line.split()
             list_1.append(s[0])
             list_2.append(s[1])
-    list_1.sort()
-    list_2.sort()
 
 if __name__ == "__main__":
     read_input()
 
-    for i in range(len(list_1)):
-        distance += abs(int(list_1[i]) - int(list_2[i]))
-    
-    print(distance)
+    for x in list_1:
+        list_2_Occurences = list_2.count(x)
+        similarity_score += int(x) * list_2_Occurences
+    print(similarity_score)
+

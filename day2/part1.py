989079fdb03c9072d7e129d9f53b953d7126346b
sophielmu <sophiemarshall-unitt@hotmail.com>
2024-12-07 20:29:29 +0000

    WIP Day 2 part 1

---
+++ day2/part1.py
@@ -0,0 +1,9 @@
+number_of_safe_reports = 0
+
+if __name__ == "__main__":
+       with open("day1/input.txt", "r") as f:
+        for line in f:
+            report = [int(x) for x in line.split()]
+
+
+

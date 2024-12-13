0988d9918b8f50b52645603b4c765dea489d1b7f
sophielmu <sophiemarshall-unitt@hotmail.com>
2024-12-13 20:03:04 +0000

    Push day 3 part 2 nearly complete WIP

--- day3/part2.py
+++ day3/part2.py
@@ -6,15 +6,14 @@ mul_string_regex = "mul\\(\\d+,\\d+\\)"
 
 if __name__ == "__main__":
     with open("day3/input.txt", "r") as f:
-        input_text = f.read()
-        first_string = re.search(".+?(?=don't\(\))", input_text).string
-        # last_string = re.search(".+?(?=don't\(\))") # todo something
-        matches = re.findall(mul_string_regex, first_string)
-        cleansed_input = input_text.removeprefix(first_string)
-        do_dont_strings = re.findall("do\(\).+?(?=don't\(\))", cleansed_input)
+        input_text = "do()" + f.read()
+        input_text = input_text + "don't()"
+        do_dont_strings = re.findall("do\(\).+?(?=don't\(\))", input_text)
 
         for string in do_dont_strings:
-            matches.append(re.findall(mul_string_regex, f.read()))
+            string_match = re.findall(mul_string_regex, string)
+            for mul_match in string_match:
+                matches.append(mul_match)
 
         for match in matches:
             numbers = re.findall("\\d+", match)
